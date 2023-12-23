631e9851039ef510704106777de401097f1a4b97
gauravsahu255 <154047872+gauravsahu255@users.noreply.github.com>
2023-12-23 14:53:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ OWNER_ID = list(map(int, getenv("OWNER_ID", "2145093972").split()))
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Noob-Mukesh/MukeshMusic")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://t.me/Andi_mandi_sandi_tu_hai_randi/13")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
@@ -76,7 +76,7 @@ clean = {}
 autoclean = []
 
 
-START_IMG_URL = getenv("START_IMG_URL", "https://te.legra.ph/file/c7ca84879787e0719c002.jpg")
+START_IMG_URL = getenv("START_IMG_URL", "https://telegra.ph/file/209cfe22be5ae3381fbb3.jpg")
 
 PING_IMG_URL = getenv(
     "PING_IMG_URL",
